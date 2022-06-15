7f09f9f465ad7a14609da6d7448b9e97423b18a2
Enrik <Enrik.n1rvan@gmail.com>
2022-06-15 22:01:15 +0300

    added quests seminar 2

---
+++ Quest_11/Program.cs
@@ -0,0 +1,6 @@
+﻿void distance (int x, int y, int x1, int y1) {
+
+    double result = Math.Sqrt((x1 - x)*(x1 - x) + (y1 - y)*(y1 - y)) ;
+    Console.WriteLine(result);
+}
+distance(3,7,8,9);
---
+++ Quest_12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void tabl (int N) {
+    for (int i = 0; i <= N; i++) {
+        int num = i * i;
+        Console.WriteLine($"{num}");
+    }
+}
+tabl(N);
