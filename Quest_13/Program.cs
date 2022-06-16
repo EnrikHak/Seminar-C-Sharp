65e6cfee2c281a9325db6852253f9e5d89540d90
Enrik <Enrik.n1rvan@gmail.com>
2022-06-16 22:06:34 +0300

    add quest 13

---
+++ Quest_13/Program.cs
@@ -0,0 +1,11 @@
+﻿int sum (int a) {
+    int result = 0;
+    for (int i = 0; i <= a; i++) {
+        result = result + i;
+    }
+    return result;
+}
+
+Console.WriteLine(sum(53));
+Console.WriteLine(sum(10));
+Console.WriteLine(sum(34));
