85d2809a7a747f956c579b561a45350ffb344ce7
Enrik <Enrik.n1rvan@gmail.com>
2022-06-16 22:06:44 +0300

    add quest 14

---
+++ Quest_14/Program.cs
@@ -0,0 +1,11 @@
+﻿int cifri (int number) {
+    int result = 0;
+
+    char[] num = number.ToString().ToCharArray();
+    for (int i = 0; i < num.Length; i++) {
+        result = result + 1;
+    }
+    return result;
+}
+
+Console.WriteLine(cifri(1321111));
