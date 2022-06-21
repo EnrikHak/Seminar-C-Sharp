64de313274ee16c3a9d58a066edea2f46d398675
Enrik <Enrik.n1rvan@gmail.com>
2022-06-21 17:17:27 +0300

    add quest 19 bool answer if number in massive

---
+++ Quest_19/Program.cs
@@ -0,0 +1,40 @@
+﻿// Создать массив
+// Определить, есть ли заданное число в  массиве
+//
+int[] FillArray() {
+
+    int[] array = new int [10];
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(-99,1000);
+    }
+    return array;
+}
+
+int[] arr = FillArray();
+
+void PrintArray(int[] array) {
+    for(int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+void InputNum(int[] array) {
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int Bool = 0;
+    for(int i = 0; i < array.Length; i++) {        
+        if (array[i] == number) {
+            Console.WriteLine("Yes!");
+            Bool = 1;
+            break;
+        }
+    }
+    if (Bool != 1) {
+            Console.WriteLine("No!");
+    }
+
+}
+
+PrintArray(arr);
+InputNum(arr);
