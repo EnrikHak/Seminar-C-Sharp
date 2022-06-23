8be36bfda48c4f27e866622bbf037713700af3cc
Enrik <Enrik.n1rvan@gmail.com>
2022-06-23 22:22:46 +0300

    add quest 23 triengle be or not to be

---
+++ Quest_23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Проверим может ли введенные числа быть сторонами треугольника");
+
+Console.WriteLine("Введите число 1: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 3: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+if (first < second + third && second < first + third && third < first + second) {
+    Console.Write("Может быть треугольником");
+} else {
+    Console.WriteLine("Не может быть треугольником");
+}
+Console.WriteLine();
