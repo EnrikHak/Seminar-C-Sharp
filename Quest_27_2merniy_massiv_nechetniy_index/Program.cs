76058a118a9f85d11be1bfc778f3d73268c3ec1a
Enrik <Enrik.n1rvan@gmail.com>
2022-07-01 13:03:40 +0300

    2merniy massiv nechetnie chisla stavim v sqrt

---
+++ Quest_27_2merniy_massiv_nechetniy_index/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[,] array) {
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(int[,] array) {
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void ChangeArray(int[,] array) {
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            if (i % 2 == 1 && j % 2 == 1) {
+               array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+}
+
+int[,] arr = new int[3, 4];
+
+
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+FillArray(arr);
+ChangeArray(arr);
+Console.WriteLine();
+FillArray(arr);
+PrintArray(arr);
+
+
+
